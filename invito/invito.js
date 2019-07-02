e35fcfb8b537451573fce0065c043e2931c977a0
davidemvf <davidemv.fiore@gmail.com>
2019-07-02 20:35:05 +0200

    Completato

--- invito/invito.js
+++ invito/invito.js
@@ -1,14 +1,13 @@
 // Controlla se l'email inserita dall'utente appartiene alla lista
 var utente = prompt("Inserisci Email:");
 var iscritti = ["uno@hotmail.it", "due@gmail.com", "tre@icloud.com", "quattro@hotmail.it", "cinque@gmail.com"];
-
+var antipatico = "Non sei invitato";
 
 for (i=0; i<=4; i++){
   if (iscritti[i] == utente){
-    document.writeln("Sei stato invitato!");
-  }
+    antipatico = "Sei stato invitato!";
 
-  else{
-    document.writeln("No email, no party");
   }
 }
+
+document.writeln(antipatico);
